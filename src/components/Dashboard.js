913c2268d568ad0a514004763cb1a020deb7b272
Anastasia <z.anastasia11@gmail.com>
2023-07-12 18:26:24 -0700

    Add the componentDidMount and componentDidUpdate lifecycle methods to the Dashboard component.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,32 +29,57 @@ const data = [
   }
 ];
 
-const panels = data.map(panel => (
-<Panel
 
-    key={panel.id}
-    id={panel.id}
-    label={panel.label}
-    value={panel.value}
-    />
-
-));
  
 
 
-
-
-
 class Dashboard extends Component {
-  state = { loading: false }
+  state = { 
+    loading: false,
+    focused:null
+   }
+
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
